86c84075184525be82de8d61f5103839a9033d8e
Harsh Davda <harsh.davda11@outlook.com>
2025-01-30 03:05:17 +0530

    argcomplete fix

--- env.sh
+++ env.sh
@@ -5,6 +5,12 @@ if [ -z "$RAS" ]; then
     export PATH=$RDI_PATH:$PATH
 fi
 if [ ! -f "$HOME/.bash_completion.d/python-argcomplete.sh" ]; then
-    activate-global-python-argcomplete3 --user
+    if command -v activate-global-python-argcomplete3 &> /dev/null; then
+        activate-global-python-argcomplete3 --user
+        exec bash
+    elif command -v activate-global-python-argcomplete &> /dev/null; then
+            activate-global-python-argcomplete --user
+            exec bash
+    fi
 fi
 export RAS="RAS"
